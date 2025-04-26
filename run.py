a8cc6158d08b7e10db09207928dc079ad55daef6
rohithgoud30 <rohithgoud30@gmail.com>
2025-04-26 05:05:49 -0400

    Fix(run): update default port to 8080 for Cloud Run compatibility

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import os
 import uvicorn
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run(
         "app.main:app",
         host="0.0.0.0",
