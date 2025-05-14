a6c525aaf6377b93edd7b6a319dacf55f76f005d
rohithgoud30 <rohithgoud30@gmail.com>
2025-05-14 12:50:40 -0400

    Feat(typesense): commit all changes

--- app/crud/__init__.py
+++ app/crud/__init__.py
@@ -1,4 +1,3 @@
 from app.crud.firebase_base import FirebaseCRUDBase
-from app.crud.user import user_crud
 from app.crud.document import document_crud
 from app.crud.submission import submission_crud 
